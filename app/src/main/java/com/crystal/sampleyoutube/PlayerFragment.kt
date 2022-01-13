68c68955df1520e65edff76a16b094d5952f3b81
kwonsoojeong <0401tnwjd@gmail.com>
2022-01-13 10:52:20 +0900

    Palyer swipe시 Motion 추가

---
+++ app/src/main/java/com/crystal/sampleyoutube/PlayerFragment.kt
@@ -0,0 +1,41 @@
+package com.crystal.sampleyoutube
+
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.fragment.app.Fragment
+import com.crystal.sampleyoutube.databinding.FragmentPlayerBinding
+import kotlin.math.abs
+
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+    private var binding: FragmentPlayerBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
+        binding = fragmentPlayerBinding
+
+        fragmentPlayerBinding.fragmentPlayerMotionLayout.setTransitionListener(object :
+            MotionLayout.TransitionListener {
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int,endId: Int) {
+            }
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+                //fragmentPlayerMotionLayout이 동작할 때, Main activity에 있는 Motion을 실행시킬것임
+                binding.let {
+                    (activity as MainActivity).also{ mainActivity ->
+                    mainActivity.findViewById<MotionLayout>(R.id.mainMotionLayout).progress = abs(progress)
+                    }
+                }
+            }
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+            }
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
